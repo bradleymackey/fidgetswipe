40ea13363d8413926013bb637b557f82f24bba76
Bradley Mackey <bradleymackey98@googlemail.com>
2017-07-07 15:54:47 +0100

    added remote config boilerplate

--- fidgetswipe-ios/fidgetswipe-ios/GameManager.swift
+++ fidgetswipe-ios/fidgetswipe-ios/GameManager.swift
@@ -11,29 +11,47 @@ import Foundation
 /// Manages the overall flow of the game
 public final class GameManager {
     
-    
     /// The current state of the game
     private var currentState:GameState = .notYetBegan
     
+    /// The current game score
     private var gameScore:UInt = 0
     
     /// The expected next move of the player
     /// - note: `nil` if game is not currently playing
     private var expectedPlayerMove:Action?
     
-    func randomMove() -> Action {
+    public init() {
+        
+    }
+    
+    /// Selects a random move.
+    private func randomMove() -> Action {
         let randomNumber = arc4random_uniform(Action.totalActions)
         if let action = Action(rawValue: Int(randomNumber)) {
-            expectedPlayerMove = action
             return action
         } else {
-            fatalError("This rawValue does not equate to a vaild action")
+            fatalError("This rawValue (\(randomNumber)) does not equate to a vaild action.")
         }
+    }
+    
+    /// A simple variable that calculates the time allowed for the current turn based on the user's current store.
+    private var timeForMove:Float {
+        let time = GameConstants.shared.getTime(forCurrentScore: gameScore)
+        return Float(time)
+    }
+    
+    public func newGame() {
+    
+    }
+    
+    public func nextMove() {
+        
     }
     
     /// Player calls this when they take a move
     /// - returns: true if this was a vaild action, false if this was not, the game has now ended. Also the player's new score.
-    func take(move:Action) -> (valid:Bool,newScore:UInt) {
+    public func take(move:Action) -> (valid:Bool,newScore:UInt) {
         if move != expectedPlayerMove {
             return (false, gameScore)
         } else {
