cdb9af8ebcf21e617aeb1c392b0e475f12cba80a
Bradley Mackey <bradleymackey98@googlemail.com>
2017-07-10 15:36:40 +0100

    model and images complete android, minor changes ios

--- FidgetPhone-android/app/src/main/java/com/bradleymackey/fidgetphone/model/TurnData.java
+++ FidgetPhone-android/app/src/main/java/com/bradleymackey/fidgetphone/model/TurnData.java
@@ -1,6 +1,10 @@
 package com.bradleymackey.fidgetphone.model;
 
 
+import android.media.Image;
+
+import com.bradleymackey.fidgetphone.R;
+
 public final class TurnData implements Cloneable {
 
     /// The action that this turn requires from the user.
@@ -28,6 +32,36 @@ public final class TurnData implements Cloneable {
 
     public double getTimeForMove() {
         return timeForMove;
+    }
+
+    // Returns the image ID for the image that this action represents.
+    public int getImageId() {
+        switch (action) {
+            case TAP:
+                return R.drawable.tap;
+            case SWIPE_UP:
+                return R.drawable.swipe_up;
+            case SWIPE_LEFT:
+                return R.drawable.swipe_left;
+            case SWIPE_DOWN:
+                return R.drawable.swipe_down;
+            case SWIPE_RIGHT:
+                return R.drawable.swipe_right;
+            case SHAKE:
+                return R.drawable.shake;
+            case UPSIDE_DOWN:
+                return R.drawable.upside_down;
+            case VOLUME_UP:
+                return R.drawable.volume_up;
+            case VOLUME_DOWN:
+                return R.drawable.volume_down;
+            case FACE_UP:
+                return R.drawable.face_up;
+            case FACE_DOWN:
+                return R.drawable.face_down;
+            default:
+                throw new RuntimeException("There is no image ID for this action!");
+        }
     }
 
     public TurnData clone() {
